de9f5732d95359572424547bd2d303f1891be711
Michał Krawczykowski <mkrawczykowski@gmail.com>
2023-03-10 22:38:12 +0100

    Layout + centered

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
 import "../styles/reset.scss";
 import "../styles/globals.scss";
+import Layout from '../components/Layout/Layout';
 
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
-}
+    return (
+        <Layout>
+            <p>test</p>
+            <Component {...pageProps} />
+        </Layout>
+    )
+}        
