01372f825f20a59c9ac74a9e21b3007208d52414
Michał Krawczykowski <mkrawczykowski@gmail.com>
2023-03-10 23:18:27 +0100

    Textarea2015

---
+++ components/Textarea2015/Textarea2015.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import style from './Textarea2015.module.scss';
+
+const Textarea2015 = () => {
+    return(
+        <textarea className={style.textarea2015}></textarea>
+    )
+}
+
+export default Textarea2015;
