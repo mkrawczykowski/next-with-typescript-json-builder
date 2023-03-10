f078ab733c00d738fe533d3b23aefaf1584cc438
Michał Krawczykowski <mkrawczykowski@gmail.com>
2023-03-10 23:19:00 +0100

    List component & sub component

---
+++ components/List1980/List1980.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import style from './list1980.module.scss';
+
+const List1980 = () => {
+    return(
+        <div className={style.list1980}>
+            <ListItem1980></ListItem1980>
+        </div>
+    )
+}
---
+++ components/ListItem1980/ListItem1980.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import style from './listItem1980.module.scss';
+
+const ListItem1980 = () => {
+    return(
+        <div className={style.listItem1980}>
+            <input type="text" />
+        </div>
+    )
+}
