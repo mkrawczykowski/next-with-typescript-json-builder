862ffe6d97f71afb19e342a52c9939beee10ebad
Michał Krawczykowski <mkrawczykowski@gmail.com>
2023-03-11 17:25:58 +0100

    ListItem1980 component

--- components/ListItem1980/ListItem1980.tsx
+++ components/ListItem1980/ListItem1980.tsx
@@ -4,7 +4,17 @@ import style from './listItem1980.module.scss';
 const ListItem1980 = () => {
     return(
         <div className={style.listItem1980}>
-            <input type="text" />
+            <div className={style.listItem1980__column}>ARG</div>
+            <div className={style.listItem1980__column}>Argentina</div>
+            <div className={style.listItem1980__column}>49</div>
+            <div className={style.listItem1980__column}>46</div>
+            <div className={style.listItem1980__column}>56</div>
+            <div className={style.listItem1980__column}>86</div>
+            <div className={style.listItem1980__column}>20</div>
+            <div className={style.listItem1980__column}>62</div>
+            <div className={style.listItem1980__column}><input type="text" className={style.listItem1980__inputText}/></div>
         </div>
     )
 }
+
+export default ListItem1980;
